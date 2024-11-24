2152fed8ae88b23fd2bab72f9e38960ae0aa8a0c
Mattia Luisi <luisimattia@itis-molinari.eu>
2024-11-24 15:04:43 +0100

    aggiunta commenti checkLogin.js

--- componenti/checkLogin.js
+++ componenti/checkLogin.js
@@ -1,4 +1,6 @@
 export const checkLogin = () => {
+  //Prende dal session storage la chiave mattiaLogin
     const log = sessionStorage.getItem("mattiaLogin");
+    //Fa il return di log
     return log == "utenteLoggato";
   }
